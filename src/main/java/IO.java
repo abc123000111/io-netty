e3551e5c540fe0a09b7c23a0effd30747b267d66
三树 <sanshu.lw@antgroup.com>
2020-08-10 16:27:39 +0800

    Initial commit

---
+++ src/main/java/IO.java
@@ -0,0 +1,36 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+public class IO {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG,100)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new StringEncoder());
+                        pipeline.addLast(new StringDecoder());
+                    }
+                });
+        ChannelFuture channelFuture = serverBootstrap.bind(new InetSocketAddress("localhost",8080)).sync();
+        channelFuture.channel().closeFuture().sync();
+        bossGroup.shutdownGracefully();
+        workGroup.shutdownGracefully();
+    }
+}
